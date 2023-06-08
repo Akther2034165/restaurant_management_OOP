76c1b9cd1daab20f5a9cde699119cb47f13c840c
unknown <Akther2034165>
2023-06-08 23:40:59 +0600

    first commit

---
+++ concept.py
@@ -0,0 +1,22 @@
+"""
+classes
+1. User : abstract base class
+2. Custer (User)
+3. Employee (User)
+4. Chef(Employee)
+5. Server(Employee)
+6. Manager(Employee)
+7. Cleaner(Employee)
+8. Supplier(User)
+9. Marketter(User)
+
+1. FoodItem(ABS)
+2. Pizza(FoodItem)
+3. Burger(FoodItem)
+4. Drink(FoodItem)
+5. Juice(FoodItem)
+6. Salad(FoodItem)
+
+1. Order
+2. Restaurant
+"""
